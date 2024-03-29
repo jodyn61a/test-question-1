858fbccc1d978a461d8bb25f7296a9df5f889dd1
Szymon Krzysztofik <szymon.krzysztofik@r.pl>
2024-03-29 11:08:16 +0100

    test-clear-project

--- App.js
+++ App.js
@@ -1,57 +1,14 @@
 import axios from "axios";
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-const splitValue = [1, 2, 3, 4, 5];
+
 export default function App() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    axios.get("https://r.pl/api/v4.3/banery/pobierz").then((e) => {
-      const flatData = e.data.flatMap((e) => e.Bloczki);
-      const c = flatData.reduce((acc, e) => {
-        splitValue.forEach((element) => {
-          const a = e.Cena.CenaAktualna % element;
-
-          if (acc[a]) {
-            acc[a].push(e);
-          } else {
-            acc[a] = [e];
-          }
-        });
-        return acc;
-      }, {});
-      setData(c);
-    });
-  }, []);
+  axios.get("https://r.pl/api/v4.3/banery/pobierz").
+ 
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {data &&
-          Object.keys(data).map((e) =>
-            data[e]?.map((el, index) => (
-              <View
-                key={e + el.BazoweInformacje.OfertaNazwa + index}
-                style={{
-                  backgroundColor:
-                    e === "1"
-                      ? "red"
-                      : e === "2"
-                      ? "blue"
-                      : e === "3"
-                      ? "green"
-                      : e === "4"
-                      ? "yellow"
-                      : "white",
-                }}
-              >
-                <Text>{el.BazoweInformacje.OfertaNazwa}</Text>
-              </View>
-            ))
-          )}
-      </ScrollView>
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
