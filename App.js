c05bf35a22d4e2fa74a0575b7b6de1abe40f3622
Szymon Krzysztofik <szymon.krzysztofik@r.pl>
2024-03-29 11:07:17 +0100

    core test

--- App.js
+++ App.js
@@ -1,10 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
+const splitValue = [1, 2, 3, 4, 5];
 export default function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios.get("https://r.pl/api/v4.3/banery/pobierz").then((e) => {
+      const flatData = e.data.flatMap((e) => e.Bloczki);
+      const c = flatData.reduce((acc, e) => {
+        splitValue.forEach((element) => {
+          const a = e.Cena.CenaAktualna % element;
+
+          if (acc[a]) {
+            acc[a].push(e);
+          } else {
+            acc[a] = [e];
+          }
+        });
+        return acc;
+      }, {});
+      setData(c);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        {data &&
+          Object.keys(data).map((e) =>
+            data[e]?.map((el, index) => (
+              <View
+                key={e + el.BazoweInformacje.OfertaNazwa + index}
+                style={{
+                  backgroundColor:
+                    e === "1"
+                      ? "red"
+                      : e === "2"
+                      ? "blue"
+                      : e === "3"
+                      ? "green"
+                      : e === "4"
+                      ? "yellow"
+                      : "white",
+                }}
+              >
+                <Text>{el.BazoweInformacje.OfertaNazwa}</Text>
+              </View>
+            ))
+          )}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +59,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
